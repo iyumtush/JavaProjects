0155293147f3e6138c279a7892719ecf5941cc88
iyumtush <tusharcse0@gmail.com>
2026-07-28 17:23:51 +0530

    Created WebSockets

--- src/BankClient.java
+++ src/BankClient.java
@@ -1,9 +1,21 @@
 
+import java.net.*;
+import java.io.*;
+
 public class BankClient {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws UnknownHostException, IOException 
+	{
+       Socket socket = new Socket("Localhost",9999);
+       BufferedReader in = new BufferedReader( 
+				new InputStreamReader(socket.getInputStream()));
+		PrintWriter out = 
+				new PrintWriter(socket.getOutputStream(),true);
+		out.println("3000");
+		String response = in.readLine();
+		System.out.println(response);
+		socket.close();
+		
 	}
 
 }
